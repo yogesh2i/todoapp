3bcd66c7691fa2f314cb32acbd8b80a1281f7d9f
yogesh <yogirao8440@gmail.com>
2023-03-26 18:33:20 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-   
-    <App />
+   <HashRouter>
+
+     <App />
+   </HashRouter>
 
 
 )
